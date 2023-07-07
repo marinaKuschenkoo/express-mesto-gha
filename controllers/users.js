7652b321b4b3db18b5b3c3ce3b4b0495a48c72de
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-07 20:51:36 +0300

    ПР13

--- controllers/users.js
+++ controllers/users.js
@@ -7,33 +7,18 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findById(req.user._id, { lean: false }, { runValidators: true })
+  User.findById(req.params._id)
     .orFail(() => { throw new Error('NotFound'); })
     .then((user) => res.status(200).send({ user }))
-    // .catch((err) => {
-    //   if (err.message === 'NotFound') {
-    //     res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
-    //     return;
-    //   }
-    //   if (err.name === 'CastError') {
-    // eslint-disable-next-line max-len
-    //     res.status(400).send({ message: 'Переданы некорректные данные при поиске пользователя' });
-    //   } else {
-    //     res.status(500).send({ message: 'Ошибка по умолчанию' });
-    //   }
-    // });
     .catch((err) => {
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
-        res.status(400).send({
-          message: 'переданы некорректные данные',
-          err: err.message,
-        });
-      } else if (err.status === 404) {
-        res.status(404).send({ message: 'Пользователь не найден' })
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+        return;
+      }
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при поиске пользователя' });
       } else {
-        res.status(500).send({
-          message: 'Что-то не так',
-        });
+        res.status(500).send({ message: 'Ошибка по умолчанию' });
       }
     });
 };
