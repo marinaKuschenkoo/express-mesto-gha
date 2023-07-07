17b7fc5ee867ae6a14d231ef6f05bb73e7fa8ca0
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-07 20:30:22 +0300

    ПР13

--- controllers/users.js
+++ controllers/users.js
@@ -2,14 +2,14 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ users }))
+    .then((users) => res.status(200).send({ users }))
     .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.user._id)
     .orFail(() => { throw new Error('NotFound'); })
-    .then((user) => res.send({ user }))
+    .then((user) => res.status(200).send({ user }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
@@ -27,7 +27,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((users) => res.send({ users }))
+    .then((users) => res.status(200).send({ users }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
@@ -45,7 +45,7 @@ module.exports.updateProfile = (req, res) => {
     runValidators: true, // данные будут валидированы перед изменением
   })
     .orFail(() => { throw new Error('NotFound'); })
-    .then((user) => res.send({ user }))
+    .then((user) => res.status(200).send({ user }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
@@ -63,7 +63,7 @@ module.exports.updateAvatar = (req, res) => {
 
   User.findByIdAndUpdate(req.user._id, { avatar })
     .orFail(() => { throw new Error('NotFound'); })
-    .then((user) => res.send({ user }))
+    .then((user) => res.status(200).send({ user }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
