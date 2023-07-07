4592a594d4610d617cf53c54f28a14e35d95c8f2
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-07 22:00:17 +0300

    ПР13

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const {
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:_id', getUserById);
+router.get('/users/:userId', getUserById);
 router.post('/users', createUser);
 router.patch('/users/me', updateProfile);
 router.patch('/users/me/avatar', updateAvatar);
