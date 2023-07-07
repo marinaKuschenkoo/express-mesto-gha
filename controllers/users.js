29712109d114e5224e6408ecd1909ebb03aac0fa
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-07 22:17:59 +0300

    ПР13

--- controllers/users.js
+++ controllers/users.js
@@ -45,7 +45,7 @@ module.exports.updateProfile = (req, res) => {
     runValidators: true, // данные будут валидированы перед изменением
   })
     .orFail(() => { throw new Error('NotFound'); })
-    .then((user) => res.status(200).send({ user }))
+    .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
@@ -63,7 +63,7 @@ module.exports.updateAvatar = (req, res) => {
 
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail(() => { throw new Error('NotFound'); })
-    .then((user) => res.status(200).send({ user }))
+    .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.message === 'NotFound') {
         res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
