f94c5703ac41205cb5af32d60b9a87a25ca8d47f
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-09 16:46:33 +0300

    13

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use('/', userRouter);
-app.use('/', cardRouter);
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страницы не существует' });
-});
 app.use((req, res, next) => {
   req.user = {
     _id: '64a5ea224e759e5c95e7f9e3',
@@ -25,7 +20,11 @@ app.use((req, res, next) => {
 
   next();
 });
-
+app.use('/', userRouter);
+app.use('/', cardRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страницы не существует' });
+});
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Сервер запущен');
