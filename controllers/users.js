2f7bca4eabacc192829d90b6d5acb98e0eefb0d6
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-09 14:12:45 +0300

    gh13

--- controllers/users.js
+++ controllers/users.js
@@ -55,16 +55,20 @@ module.exports.updateProfile = (req, res) => {
     });
 };
 module.exports.updateAvatar = (req, res) => {
+  const userId = req.user._id;
   const { avatar } = req.body;
-
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.status(200).send(user))
+  // eslint-disable-next-line max-len
+  User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
+    .then((user) => {
+      res.send({ user });
+    })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({
-          message: 'Переданы некорректные данные при обновлении аватара.',
-        });
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден.' });
+      } else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+      } else {
+        res.status(500).send({ message: `На сервере произошла ошибка: ${err.name}` });
       }
-      return res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
