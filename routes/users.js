77236ce967e81e352c3a080938bfb9128612bc7a
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-09 15:20:17 +0300

    13

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const {
   getUsers,
-  getUserById,
+  getUser,
   createUser,
-  updateProfile,
+  updateUser,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUserById);
+router.get('/users/:userId', getUser);
 router.post('/users', createUser);
-router.patch('/users/me', updateProfile);
+router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
