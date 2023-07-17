816d839da890fb6bf41a059c410f4786f76adaf9
Kuschenko Marina <marinakuschenko1@yandex.ru>
2023-07-17 16:04:49 +0300

    ПР14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,21 +4,18 @@ const jwt = require('jsonwebtoken');
 const ValidationError = require('../errors/ValidationError');
 const { secret } = require('../constants');
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (req, res, next, err) => {
+module.exports = (req, res, next) => {
   console.log(secret);
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    // return err.message;
     throw new ValidationError('Необходима авторизация!');
-    // res.status(VALIDATION_ERROR).send({ message: 'Необходима авторизация!' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, secret);
   } catch (error) {
-    throw new ValidationError('Необходима авторизация!');
+    throw new ValidationError('Необходима авторизация');
   }
   req.user = payload;
   next();
